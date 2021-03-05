dc7aed449481efaf9de021247c49675a3eaac3f2
anya bron <anyabron8@gmail.com>
2021-03-05 18:44:39 +0200

    Added dagger/hilt
    Added module dependency injection framework for avoid coupling between classes and to achieve easier and cleaner flow in the app.

---
+++ app/src/main/java/com/anya/dogipedia/DogsApp.kt
@@ -0,0 +1,7 @@
+package com.anya.dogipedia
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DogsApp : Application()
---
+++ app/src/main/java/com/anya/dogipedia/inject/AppModule.kt
@@ -0,0 +1,11 @@
+package com.anya.dogipedia.inject
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+}
