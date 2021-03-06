b06d71e8aef28b44489861770a1ab5d0beedd118
anya bron <anyabron8@gmail.com>
2021-03-06 18:24:59 +0200

    Viewmodel factory and injection for main activity

--- app/src/main/java/com/anya/dogipedia/MainActivity.kt
+++ app/src/main/java/com/anya/dogipedia/MainActivity.kt
@@ -3,7 +3,9 @@ package com.anya.dogipedia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.anya.dogipedia.ui.breeds.BreedsFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
